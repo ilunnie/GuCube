0c9e235a9c14ccaf5bc83744339b5607f162e586
ilunnie <luisgustavocaris2004@gmail.com>
2023-11-22 16:58:31 -0300

    22.11.2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, MatIconModule, MatButtonModule, MatSelectModule]
 })
 export class AppComponent {
   title = 'GuCube';
+  isLogged = true;
+  isAdm = false;
+  showFilter = false;
+
+
+  countries = ['Brasil', 'Argentina', 'Chile'];
+  country: string = "-- Coutry --";
+
+  clickFilter() {
+    this.showFilter = !this.showFilter;
+  }
 }
